4a7f0303e6f6dffc21b6ec177e891cbf5d68450d
Jefferson Garcia <Jefferson Garcia>
2024-02-26 13:48:49 +0800

    Add decrement

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,24 +2,38 @@ import "package:flutter_test/flutter_test.dart";
 import "package:fluttertestexample/counter.dart";
 
 main() {
-  test('Given counter class when intantiated, then count = 0', () {
-    //Arrange
-    Counter counter;
-    //Act
-    counter = Counter();
-    int count = counter.count;
-    //Assert
-    expect(count, 0);
-  });
+  group('Counter Class:', () {
+    late Counter counter;
+    setUp(() => counter = Counter());
+    test('Given counter class when intantiated, then count = 0', () {
+      //Arrange
 
-  test(
-      'Given a Counter class  is instantiated, when incrementCount is called, then count = 1',
-      () => () {
-            //Arrange
-            Counter counter = Counter();
-            //Act
-            counter.incrementCount();
-            //Assert
-            expect(counter.count, 1);
-          });
+      //Act
+      counter = Counter();
+      int count = counter.count;
+      //Assert
+      expect(count, 0);
+    });
+
+    test(
+        'Given a Counter class  is instantiated, when incrementCount is called, then count = 1',
+        () {
+      //Arrange
+
+      //Act
+      counter.incrementCount();
+      //Assert
+      expect(counter.count, 1);
+    });
+    test(
+        'Given a Counter class  is instantiated, when decrementCount is called, then count = -1',
+        () {
+      //Arrange
+
+      //Act
+      counter.decrementCount();
+      //Assert
+      expect(counter.count, -1);
+    });
+  });
 }

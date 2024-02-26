103f080e616290b0c4d270f7c13a265d68ed8ed5
james <james.jumawan0912@gmail.com>
2024-02-26 14:55:49 +0800

    feat: update counter increment and decrement functions

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -16,24 +16,44 @@ main() {
     });
 
     test(
-        'Given a Counter class  is instantiated, when incrementCount is called, then count = 1',
+        'Given a Counter class  is instantiated, when incrementCount is called 4 times, then count is incremented into 1, 3, 6, 10',
         () {
       //Arrange
 
       //Act
       counter.incrementCount();
-      //Assert
       expect(counter.count, 1);
+
+      counter.incrementCount();
+      expect(counter.count, 3);
+
+      counter.incrementCount();
+      expect(counter.count, 6);
+
+      counter.incrementCount();
+      expect(counter.count, 10);
     });
     test(
-        'Given a Counter class  is instantiated, when decrementCount is called, then count = -1',
+        'Given a Counter class  is instantiated, when decrementCount is called, then count = 0',
         () {
       //Arrange
 
       //Act
       counter.decrementCount();
       //Assert
-      expect(counter.count, -1);
+      expect(counter.count, 0);
+    });
+    test(
+        'Given a Counter class  is instantiated, when incrementCount is called twice and decrementCount is called, then becomes 1, 3, 2',
+        () {
+      counter.incrementCount();
+      expect(counter.count, 1);
+
+      counter.incrementCount();
+      expect(counter.count, 3);
+
+      counter.decrementCount();
+      expect(counter.count, 2);
     });
     test(
         'Given a Counter class is instantiated, when incrementCount is called and reset is called, then the count = 0',
